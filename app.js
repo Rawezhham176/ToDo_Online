8d6bca596f58738ee4069fb56c4a3c283ba999b6
Rawezh Hama <Rawezh.Hama@otto.de>
2021-10-17 23:57:34 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newTask input').value == 0) {
+        window.alert("Please enter your Task!?")
+    } else {
+        document.querySelector("#tasks").innerHTML +=
+            `
+    <div class="task">
+        <span id="taskname">
+            ${document.querySelector('#newTask input').value}
+        </span>
+        <button class="delete">
+            <i class="far fa-trash-alt" ></i>
+        </button>
+    </div>
+    `;
+
+        var current_task = document.querySelectorAll(".delete");
+        for (var i = 0; i < current_task.length; i++) {
+            current_task[i].onclick = function () {
+                this.parentNode.remove()
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for (var i = 0; i < tasks.length; i++) {
+            tasks[i].onclick = function () {
+                this.classList.toggle("completed")
+            }
+        }
+
+        document.querySelector('#newTask input').value = ""
+
+    }
+}
